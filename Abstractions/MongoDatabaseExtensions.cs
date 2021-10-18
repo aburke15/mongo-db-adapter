0e849ad4271991c8d5921a5bf0206e4bbb90f183
Andre Burke <aburke.dev@gmail.com>
2021-10-17 21:50:55 -0600

    added mongo db client

--- Abstractions/MongoDatabaseExtensions.cs
+++ Abstractions/MongoDatabaseExtensions.cs
@@ -1,13 +1,34 @@
 
+using System;
+using Ardalis.GuardClauses;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDatabaseAdapter.Options;
+using MongoDB.Driver;
 
 namespace MongoDatabaseAdapter.Abstractions
 {
     public static class MongoDatabaseExtensions
     {
-        public static IServiceCollection AddMongoDb(this IServiceCollection services, IConfiguration configuration)
+        /// <returns>The IServiceCollection so that additional calls can be chained.</returns>
+        public static IServiceCollection AddMongoDb(this IServiceCollection services, Action<AddMongoDbOptions> setupAction)
         {
+            services = Guard.Against.Null(services, nameof(services));
+            setupAction = Guard.Against.Null(setupAction, nameof(setupAction));
+
+            services
+                .AddOptions()
+                .Configure(setupAction);
+
+            services.AddScoped<IMongoClient, MongoClient>(provider => 
+            {
+                var options = provider.GetRequiredService<IOptions<AddMongoDbOptions>>().Value;
+                var connectionString = Guard.Against.NullOrWhiteSpace(options?.ConnectionString, nameof(options.ConnectionString));
+
+                return new MongoClient(connectionString);
+            });
+            
             return services;
         }
     }
