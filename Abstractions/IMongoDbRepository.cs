ee0a1b248e69769eab4ad391126b22952663d5d5
Andre Burke <aburke.dev@gmail.com>
2021-10-20 17:38:43 -0600

    added get all method in repository

--- Abstractions/IMongoDbRepository.cs
+++ Abstractions/IMongoDbRepository.cs
@@ -1,8 +1,11 @@
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace MongoDatabaseAdapter.Abstractions
 {
     public interface IMongoDbRepository
     {
-        
+        Task<IEnumerable<T>> GetAllAsync<T>(string databaseName, string collectionName);
     }
 }
--- Abstractions/MongoDbRepository.cs
+++ Abstractions/MongoDbRepository.cs
@@ -1,14 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
 using MongoDB.Driver;
 
 namespace MongoDatabaseAdapter.Abstractions
 {
-    internal class MongoDbRepository : IMongoDbRepository
+    public class MongoDbRepository : IMongoDbRepository
     {
         private readonly IMongoClient _client;
 
-        public MongoDbRepository(IMongoClient client)
+        public MongoDbRepository(IMongoClient client) 
+            => _client = Guard.Against.Null(client, nameof(client));
+
+        public async Task<IEnumerable<T>> GetAllAsync<T>(string databaseName, string collectionName)
         {
-            _client = client;
+            var db = _client.GetDatabase(databaseName);
+
+            return await db.GetCollection<T>(collectionName)
+                .Find(FilterDefinition<T>.Empty)
+                .ToListAsync();
         }
     }
 }
