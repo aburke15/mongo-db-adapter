72e80f645d4f0a6c01c0ee5abcc9bb8200f9b782
Andre Burke <aburke.dev@gmail.com>
2021-10-20 23:10:03 -0600

    added insert one async method

--- Abstractions/MongoDbRepository.cs
+++ Abstractions/MongoDbRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
 using MongoDB.Driver;
@@ -12,13 +13,29 @@ namespace MongoDatabaseAdapter.Abstractions
         public MongoDbRepository(IMongoClient client) 
             => _client = Guard.Against.Null(client, nameof(client));
 
-        public async Task<IEnumerable<T>> GetAllAsync<T>(string databaseName, string collectionName)
+        public async Task<IEnumerable<T>> GetAllAsync<T>(string databaseName, string collectionName, CancellationToken ct = default)
         {
+            ValidateMethodParameters(databaseName, collectionName);
             var db = _client.GetDatabase(databaseName);
 
             return await db.GetCollection<T>(collectionName)
-                .Find(FilterDefinition<T>.Empty)
-                .ToListAsync();
+                .Find(Builders<T>.Filter.Empty)
+                .ToListAsync(ct);
+        }
+
+        public async Task InsertOneAsync<T>(T entity, string databaseName, string collectionName, CancellationToken ct = default)
+        {
+            ValidateMethodParameters(databaseName, collectionName);
+            var db = _client.GetDatabase(databaseName);
+            
+            await db.GetCollection<T>(collectionName)
+                .InsertOneAsync(document: entity, options: null, cancellationToken: ct);
+        }
+
+        private static void ValidateMethodParameters(string databaseName, string collectionName)
+        {
+            Guard.Against.NullOrWhiteSpace(databaseName, nameof(databaseName));
+            Guard.Against.NullOrWhiteSpace(collectionName, nameof(collectionName));
         }
     }
 }
