d3fb4c501054145fa715029d16a29b37b97d87aa
Andre Burke <aburke.dev@gmail.com>
2021-11-12 22:14:41 -0700

    added find methods to repo

--- Abstractions/IMongoDbRepository.cs
+++ Abstractions/IMongoDbRepository.cs
@@ -1,13 +1,21 @@
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using MongoDatabaseAdapter.Settings;
+using MongoDB.Driver;
 
 namespace MongoDatabaseAdapter.Abstractions;
 
 public interface IMongoDbRepository
 {
+    [UsedImplicitly]
+    Task<T> FindOneAsync<T>(MongoDbConnectionSettings settings, FilterDefinition<T> filter, CancellationToken ct = default) where T : class;
+    
+    [UsedImplicitly]
+    Task<IReadOnlyList<T>> FindManyAsync<T>(MongoDbConnectionSettings settings, FilterDefinition<T> filter, CancellationToken ct = default) where T : class;
+    
     [UsedImplicitly]
     Task<IReadOnlyList<T>> GetAllAsync<T>(MongoDbConnectionSettings settings, CancellationToken ct = default) where T : class;
 
